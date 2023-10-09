325c52b13f9deab0f17b342be8d44d2ca26bf402
SunDrargon <88926252+Sundrago@users.noreply.github.com>
2023-10-10 02:29:12 +0900

    Time: 878 ms (19.48%), Space: 240.5 MB (35.53%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.cs
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.cs
@@ -1,52 +1,68 @@
-public class TimeMap {
-    private Dictionary<string, List<Data>> datas;
-    
-    public TimeMap() {
-        datas = new Dictionary<string, List<Data>>();
-    }
-    
-    public void Set(string key, string value, int timestamp) {
-        if(!datas.ContainsKey(key)) {
-            datas.Add(key, new List<Data>());
-        }
-        datas[key].Add(new Data(value, timestamp));
-    }
-    
-    public string Get(string key, int timestamp) {
-        if(!datas.ContainsKey(key)) return "";
-        
-        List<Data> valuesList = datas[key];
-        
-        int maxTimestamp = int.MinValue;
-        string maxValue = "";
-        
-        foreach(Data data in valuesList) {
-            if(data.timestamp > timestamp) continue;
+public class TimeMap
+    {
+        private readonly IDictionary<string, IList<(int timestamp, string value)>> _data;
 
-            if(data.timestamp > maxTimestamp) {
-                maxTimestamp = data.timestamp;
-                maxValue = data.value;
+        public TimeMap()
+        {
+            _data = new Dictionary<string, IList<(int timestamp, string value)>>();
+        }
+
+        public void Set(string key, string value, int timestamp)
+        {
+            if (!_data.ContainsKey(key))
+            {
+                _data[key] = new List<(int timestamp, string value)>();
             }
-        }
-        
-        return maxValue;
-    }
-    
-    
-    public class Data {
-        public string value;
-        public int timestamp;
-        
-        public Data(string _value, int _timestamp){
-            value = _value;
-            timestamp = _timestamp;
-        }
-    }
-}
 
-/**
- * Your TimeMap object will be instantiated and called as such:
- * TimeMap obj = new TimeMap();
- * obj.Set(key,value,timestamp);
- * string param_2 = obj.Get(key,timestamp);
- */
+            _data[key].Add((timestamp, value));
+        }
+
+        public string Get(string key, int timestamp)
+        {
+            if (!_data.ContainsKey(key))
+            {
+                return string.Empty;
+            }
+
+            var list = _data[key];
+
+            int left = 0;
+            int right = list.Count - 1;
+
+            while (left < right)
+            {
+                if (right - left == 1)
+                {
+                    break;
+                }
+
+                int mid = left + (right - left) / 2;
+
+                var midItem = list[mid];
+                if (midItem.timestamp == timestamp)
+                {
+                    return midItem.value;
+                }
+
+                if (midItem.timestamp < timestamp)
+                {
+                    left = mid;
+                    continue;
+                }
+
+                right = mid;
+            }
+
+            if (list[right].timestamp <= timestamp)
+            {
+                return list[right].value;
+            }
+
+            if (list[left].timestamp <= timestamp)
+            {
+                return list[left].value;
+            }
+
+            return string.Empty;
+        }
+    }
