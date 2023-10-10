90faa8c4e214c5e3256a8a3d8bf906b6d1395902
SunDrargon <88926252+Sundrago@users.noreply.github.com>
2023-10-11 01:09:40 +0900

    Time: 79 ms (60.84%), Space: 39 MB (73.62%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cs
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cs
@@ -1,12 +1,12 @@
 public class Solution {
     public int FindMin(int[] nums) {
-        for(int i=0; i<nums.Length-1; i++) {
-            if(nums[i] > nums[i+1])
-                return (nums[i+1]);
-        }
-        return nums[0];
-        
-        // Array.Sort(nums);
+        // for(int i=0; i<nums.Length-1; i++) {
+        //     if(nums[i] > nums[i+1])
+        //         return (nums[i+1]);
+        // }
         // return nums[0];
+        
+        Array.Sort(nums);
+        return nums[0];
     }
 }
